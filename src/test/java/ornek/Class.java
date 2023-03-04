a81c7f48d09a1dda43746e34a93093c769b83f04
Burhan <119176022+BurhanTastan@users.noreply.github.com>
2023-03-04 21:13:21 +0100

    first commit

---
+++ src/test/java/ornek/Class.java
@@ -0,0 +1,15 @@
+package ornek;
+
+public class Class {
+    public static void main(String[] args) {
+        System.out.println("Github calismasi yapildi");
+
+    }
+}
+
+
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/BurhanTastan/BurhanGithub.git
+        git push -u origin main
